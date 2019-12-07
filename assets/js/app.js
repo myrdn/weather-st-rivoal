304cbe4c230ef6b061b4a4ab76854a81d4c320b6
Myrdin <myrdin@clic2000.fr>
2019-12-07 17:59:53 +0100

    Wind speed in km/h

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,3 @@
-var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
 var requestURL = "http://api.openweathermap.org/data/2.5/weather?q=Saint-Rivoal,fr&APPID=***REMOVED***&units=metric";
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
@@ -10,20 +7,41 @@ request.send();
 request.onload = function () {
     var meteo = request.response;
     var temperature = meteo.main['temp'];
-    var texteTemp = document.getElementById('general');
-    texteTemp.innerHTML = "Il fait actuellement " + "<b>"+ temperature + "°C</b>";
-    var texteConseil = document.getElementById("conseils");
+    var textTemp = document.getElementById('general');
+    textTemp.innerHTML = "Il fait actuellement " + "<b>"+ temperature + "°C</b>";
+    var textConseil = document.getElementById('conseils');
     if (temperature <= 0) {
-        texteConseil.innerHTML = "Il gèle rentre ta verveine";
+        textConseil.innerHTML = "Il gèle rentre ta verveine";
+    } else if (temperature <= 6) {
+        textConseil.innerHTML = "Ça caille !";
     } else if (temperature <= 12) {
-        texteConseil.innerHTML = "Prends ton bonnet si tu vas te balader sur les crêtes."
+        textConseil.innerHTML = "Prends ton bonnet si tu vas te balader sur les crêtes."
     } else if (temperature <= 15) {
-        texteConseil.innerHTML = "Mieux vaut garder ta veste pour grimper au Menez Mikael";
+        textConseil.innerHTML = "Mieux vaut garder ta veste pour grimper au Menez Mikael";
     } else if (temperature > 15) {
-        texteConseil.innerHTML = "Un tour du Drennec en t-shirt ?";
+        textConseil.innerHTML = "Un tour du Drennec en t-shirt ?";
     } else if (temperature > 20) {
-        texteConseil.innerHTML = "En voiture direction la plage !";
+        textConseil.innerHTML = "En voiture direction la plage !";
     }
+    var wind = meteo.wind['speed'];
+    wind = Math.round(3600 * wind / 1000);
+    var textWind = document.getElementById('wind');
+    textWind.innerHTML = "Le vent souffle à " + "<b>"+ wind + "km/h</b>" ;
+    var textConseilWind = document.getElementById('conseils-wind');
+    if (wind == 0) {
+        textConseilWind.innerHTML = "Pas un brin de vent!";
+    } else if (wind <= 20) {
+        textConseilWind.innerHTML = "Tu ne risques pas de t'envoler!";
+    } else if (wind <= 40) {
+        textConseilWind.innerHTML = "Regardes les branches s'agiter...";
+    } else if (wind <= 70) {
+        textConseilWind.innerHTML = "Attention aux branches sur la route !";
+    } else if (wind > 70) {
+        textConseilWind.innerHTML = "C'est la tempête !";
+    } 
+    var rain = meteo.rain['1h'];
+    if (rain )
+    console.log(rain);
 
 }
 

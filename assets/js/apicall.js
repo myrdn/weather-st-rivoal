5e046d26b07bb7517316d5311b486b50d481e02d
Myrdin <myrdin@clic2000.fr>
2020-11-22 17:00:49 +0100

    Update gitignore

--- assets/js/apicall.js
+++
@@ -1,7 +0,0 @@
-//OpenWeatherMap API call
-const requestURL = "http://api.openweathermap.org/data/2.5/weather?q=Saint-Rivoal,fr&APPID=***REMOVED***&units=metric";
-const request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-
